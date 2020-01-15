980891dcdf65ed3dd9e55c05e410f3deb2dc34b3
meowu <474384902@qq.com>
2020-01-16 00:03:20 +0800

    implement fn runtime logger plugin.

---
+++ packages/babel-plugin-runtime-logger/src/index.js
@@ -0,0 +1,43 @@
+module.exports = function logger({ types: t}) {
+    return {
+        name: 'logger',
+        visitor: {
+            FunctionDeclaration: {
+                enter(path) {
+                    path.get('body')
+                    .unshiftContainer( /* or we can also use insertBefore/insertAfter */
+                        'body', 
+                        t.callExpression(
+                            t.memberExpression(
+                                t.identifier('console'),
+                                t.identifier('time')
+                            ),
+                            [
+                                t.stringLiteral(path.node.id.name)
+                            ]
+                        )
+                        )
+                },
+                exit(path) {
+                    const blockStatement = path.node.body;
+                    const lastExpression = blockStatement.get('body').pop();
+                    const timeEndStatement = t.callExpression(
+                        t.memberExpression(
+                            t.identifier('console'),
+                            t.identifier('timeEnd')
+                        ),
+                        [
+                            t.stringLiteral(path.node.id.name)
+                        ]
+                    )
+                    if (t.isReturnStatement(lastExpression.node)) {
+                        lastExpression.insertBefore(timeEndStatement);
+                    } else {
+                        // or lastExpression.insertAfter(timeEndStatement);
+                        blockStatement.pushContainer('body', timeEndStatement);
+                    }
+                }
+            }
+        }
+    }
+}
