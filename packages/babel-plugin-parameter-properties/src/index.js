4d2a1a668e14a54b1cb3ece5a9dedb1b71f65ee8
meowu <474384902@qq.com>
2020-01-21 00:45:01 +0800

    feat: params properties.

--- packages/babel-plugin-parameter-properties/src/index.js
+++ packages/babel-plugin-parameter-properties/src/index.js
@@ -1,10 +1,22 @@
+
+const createThisMemberExpression = (t, key) => {
+  return t.memberExpression(
+      t.thisExpression(),
+      t.identifier(key),
+  )
+};
+
+const createParamsAssignment = (t, key) => {
+    return t.assignmentExpression('=', createThisMemberExpression(t, key), t.identifier(key))
+}
+
 module.exports = function parameterProperty({ types: t }) {
     return {
         name: 'parameter-properties',
         visitor: {
             ClassDeclaration(path) {
                 // we can also detect type of decorators, and support what args can be assigned.
-                const decorators = path.node.decorators;
+                const decorators = path.node.decorators || [];
                 const assigns = decorators.filter(decorator => {
                     return t.isIdentifier(decorator.expression) && decorator.expression.name === 'AutoAssign'
                 })
@@ -12,6 +24,22 @@ module.exports = function parameterProperty({ types: t }) {
                 assigns.forEach(assign => {
                     path.node.decorators = decorators.filter(d => d !== assign);
                 });
+
+                const body = path.get('body.body');
+                // or path.isClassMethod({'kind': 'constructor'});
+                let constructor = body.find(path => path.equals('kind', 'constructor'));
+                if (!constructor) {
+                    return;
+                }
+                const params = constructor.get('params').map(id => id.node.name);
+                const funcBody = constructor.get('body');
+                params.forEach(param => {
+                    funcBody.pushContainer('body', 
+                        t.expressionStatement(
+                            createParamsAssignment(t, param)
+                        )
+                    );
+                })
             }
         }
     }
